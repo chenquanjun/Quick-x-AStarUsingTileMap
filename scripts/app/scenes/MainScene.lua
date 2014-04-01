50716e6976d3e412df521475d7554daf69350f7a
chenquanjun <chenshiyouxiang@gmail.com>
2014-04-01 12:01:03 +0800

    action

--- scripts/app/scenes/MainScene.lua
+++ scripts/app/scenes/MainScene.lua
@@ -55,56 +55,16 @@ function MainScene:ctor()
         
         self:addChild(sprite)
 
-        self:playAction(sprite, 0.3, 145, 365)
-
-        -- local indexFlag = 0 --执行标志
-        -- local mapPath = _mapInfo:findPath(145, 365) --地图路径类
-
-        -- local startPoint = mapPath:getPointAtIndex(1) --第一个点
-        -- local pointNum = mapPath:getPointArrCount()
-        
-        -- sprite:setPosition(startPoint)
-
-
-        -- print("num:"..pointNum)
-
-        -- local unitPerInterval = 10
-
-        -- local delay = CCDelayTime:create(0.03) --延迟
-        -- local callfunc = CCCallFunc:create(function()
-        --                     local index = _mapInfo:int(indexFlag / unitPerInterval) + 1  
-                            
-        --                     local curPoint = mapPath:getPointAtIndex(index)
-                               
-        --                     if index < pointNum then
-        --                         local nextPoint = mapPath:getPointAtIndex(index + 1)
-        --                         local offset = (indexFlag - (index - 1) * unitPerInterval) / unitPerInterval
-        --                         print(offset)
-        --                         local x = curPoint.x + (nextPoint.x - curPoint.x) * offset
-        --                         local y = curPoint.y + (nextPoint.y - curPoint.y) * offset
-        --                         curPoint = ccp(x, y)
-
-        --                     elseif index == pointNum then
-        --                         sprite:stopActionByTag(999) --停止
-        --                     end
-        --                     indexFlag = indexFlag + 1 --标志增加
-
-        --                     sprite:setPosition(curPoint) --设置坐标
-
-        --                     end)
-        -- local sequence = CCSequence:createWithTwoActions(delay, callfunc)
-        -- local action = CCRepeatForever:create(sequence)
-        -- action:setTag(999)
-        -- sprite:runAction(action)
+        self:playAction(sprite, 0.3, 145, 146)
     end
     
 
 end
 
---speed 为移动一格的速度
+--sprite: 精灵，speed: 移动一格的速度, startId:开始id，endId:结束id
 function MainScene:playAction(sprite, speed, startId, endId)
         local indexFlag = 0 --执行标志
-        local unitPerInterval = 10 --两个格子之间划分成10个坐标点
+        local unitDivideNum = 10 --两个格子之间划分成10个坐标点
         local actionTag = 999 --动作tag，用来停止动作
 
         local mapPath = _mapInfo:findPath(startId, endId) --地图路径类
@@ -121,17 +81,18 @@ function MainScene:playAction(sprite, speed, startId, endId)
         
         local delay = CCDelayTime:create(speed / 10) --延迟
         local callfunc = CCCallFunc:create(function()
-
-                            local index = _mapInfo:int(indexFlag / unitPerInterval) + 1  
+                            --index 从 1 开始，所以需要 +1
+                            local index = _mapInfo:int(indexFlag / unitDivideNum) + 1  
                             
                             local curPoint = mapPath:getPointAtIndex(index)
                                
                             if index < pointNum then
+                                --线性化坐标点,
                                 local nextPoint = mapPath:getPointAtIndex(index + 1)
-                                local offset = (indexFlag - (index - 1) * unitPerInterval) / unitPerInterval
+                                local offset = (indexFlag - (index - 1) * unitDivideNum) / unitDivideNum
                                 local x = curPoint.x + (nextPoint.x - curPoint.x) * offset
                                 local y = curPoint.y + (nextPoint.y - curPoint.y) * offset
-                                curPoint = ccp(x, y)
+                                curPoint = ccp(x, y) 
 
                             elseif index == pointNum then
                                 sprite:stopActionByTag(actionTag) --停止
